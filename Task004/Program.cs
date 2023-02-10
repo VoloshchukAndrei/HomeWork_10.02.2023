fee0b8eb08c0589395b5b44668eb70eba0a22695
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-10 23:53:37 +0500

    Task #4

---
+++ Task004/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine()!);
+
+int max = numberA;
+
+if(max < numberB)
+{
+    max = numberB;
+}
+if(max < numberC)
+{
+    max = numberC;
+}
+Console.Write("max = " + max);
