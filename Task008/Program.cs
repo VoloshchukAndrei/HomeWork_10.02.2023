acea7af630b38b8b45c209ef0cd2de277506d852
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-11 01:56:12 +0500

    Task #8

---
+++ Task008/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int i = 1;
+while (numberA >= i)
+{
+    if (i % 2 == 0)
+    {
+    Console.WriteLine(i);
+    }
+    i = i + 1;
+}   
