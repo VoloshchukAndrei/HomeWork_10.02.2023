c434f179ccf9777c1bde4fbb48fda68cf0861cef
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-11 00:52:54 +0500

    Task #6

---
+++ Task006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Является ли число чётным");
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA % 2 == 0)
+{
+   Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
